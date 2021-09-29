8c573fa3cded2e1b12d77ffc6a1d5f8e20bac918
Maia <diegosanchesmaia31@gmail.com>
2021-09-29 14:27:49 -0300

    Criando atribudo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,18 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
     p = Pessoa('Renzo')
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
